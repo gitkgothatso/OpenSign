54e82a2da794952ffe8186f87ea273d2c5100d10
Kgothatso <k.seboya@gmail.com>
2026-01-27 14:39:04 +0200

    chore: Remove all folders except apps/OpenSign
    - Removed all root-level files and folders
    - Removed apps/localstack, apps/mongo, apps/OpenSignServer
    - Kept only apps/OpenSign as the main frontend application

--- apps/OpenSignServer/migrationdb/index.js
+++
@@ -1,7 +0,0 @@
-import createContactIndex from './createContactIndex.js';
-import createDocumentIndex from './createDocumentIndex.js';
-
-export default async function runDbMigrations() {
-  await createContactIndex();
-  await createDocumentIndex();
-}
